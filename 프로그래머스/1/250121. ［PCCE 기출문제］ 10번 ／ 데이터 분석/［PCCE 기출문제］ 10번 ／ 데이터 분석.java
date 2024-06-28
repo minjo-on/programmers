1e6b7287962090e9f889b992884ddd6b2fc3ebc0
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-06-28 15:21:50 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 6.17 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -2,25 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int e = filter(ext);
-        int s = filter(sort_by);
+        String[] index = new String[]{"code","date","maximum","remain"};
+        int e = Arrays.asList(index).indexOf(ext);
+        int s = Arrays.asList(index).indexOf(sort_by);
 
         int[][] answer = Arrays.stream(data)
                 .filter(arr -> arr[e] < val_ext)
-                .sorted(Comparator.comparing(arr -> arr[s]))
+                .sorted(Comparator.comparingInt(arr -> arr[s]))
                 .toArray(int[][]::new);
 
         return answer;
-    }
-
-    private int filter(String s){
-        int n = 0;
-        switch (s){
-            case "code" : n = 0; break;
-            case "date" : n = 1; break;
-            case "maximum" : n = 2; break;
-            case "remain" : n = 3; break;
-        }
-        return n;
     }
 }
