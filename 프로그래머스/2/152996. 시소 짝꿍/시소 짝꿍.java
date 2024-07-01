418019b12678a95b7a7c1efd461379fb62b35041
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-07-02 07:23:09 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.41 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -4,25 +4,18 @@ import java.util.stream.Collectors;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-
         Arrays.sort(weights);
-
+        
         Map<Double, Integer> map = new HashMap<>();
 
-        for (int w : weights){
-            double a = w * 1.0;
-            double b = (w * 1.0) / 2.0;
-            double c = (w * 2.0) / 3.0;
-            double d = (w * 3.0) / 4.0;
-
-            if (map.containsKey(a)) answer += map.get(a);
-            if (map.containsKey(b)) answer += map.get(b);
-            if (map.containsKey(c)) answer += map.get(c);
-            if (map.containsKey(d)) answer += map.get(d);
-            
-            map.put((w * 1.0),map.getOrDefault((w*1.0),0)+1);
+        for (double w : weights){
+            answer += map.getOrDefault(w,0);
+            answer += map.getOrDefault(w/2,0);
+            answer += map.getOrDefault(w*2/3,0);
+            answer += map.getOrDefault(w*3/4,0);
+            map.put(w,map.getOrDefault(w,0)+1);
         }
-        
+
         return answer;
     }
 }
