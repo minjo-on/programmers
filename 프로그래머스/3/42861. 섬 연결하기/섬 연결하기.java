2ebba1255addf74a30b4574ebb9527b2c86c5342
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-07-16 22:11:59 +0900

    [level 3] Title: 섬 연결하기, Time: 0.77 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    int[] parent;
+    
+    private int find(int idx) {
+        if (parent[idx] == idx) {
+            return idx;
+        }
+        return parent[idx] = find(parent[idx]);
+    }
+
+    private void union(int idx1, int idx2) {
+        int root1 = find(idx1);
+        int root2 = find(idx2);
+        if (root1 != root2) {
+            parent[root2] = root1;
+        }
+    }
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        parent = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        for (int[] cost : costs) {
+            if (find(cost[0]) != find(cost[1])) {
+                union(cost[0], cost[1]);
+                answer += cost[2];
+            }
+        }
+
+        return answer;
+    }
+}
