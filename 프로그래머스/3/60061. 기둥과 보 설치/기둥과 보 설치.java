37779f1aead2ae2f0981ab9116928099d244cff5
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-08-03 02:46:30 +0900

    [level 3] Title: 기둥과 보 설치, Time: 200.09 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class Solution {
     public int[][] solution(int n, int[][] build_frame) {
         List<int[]> builds = new ArrayList<>();
-        Set<List<Integer>> available = new HashSet<>();
 
-        for(int[] build : build_frame) {
+        for (int[] build : build_frame) {
             int x = build[0];
             int y = build[1];
             int type = build[2];
@@ -13,18 +12,12 @@ public class Solution {
 
             if (action == 1) {
                 builds.add(new int[]{x, y, type});
-                
-                if (isValid(builds)) {
-                    available.add(Arrays.asList(x, y, type));
-                } else {
+                if (!canRemain(builds)) {
                     builds.remove(builds.size() - 1);
                 }
-            } else { 
+            } else {
                 builds.removeIf(b -> b[0] == x && b[1] == y && b[2] == type);
-                
-                if (isValid(builds)) {
-                    available.remove(Arrays.asList(x, y, type));
-                } else {
+                if (!canRemain(builds)) {
                     builds.add(new int[]{x, y, type});
                 }
             }
@@ -43,38 +36,31 @@ public class Solution {
         return builds.toArray(new int[builds.size()][]);
     }
 
-    private boolean isValid(List<int[]> builds) {
+    private boolean canRemain(List<int[]> builds) {
         for (int[] build : builds) {
             int x = build[0];
             int y = build[1];
             int type = build[2];
 
-            if (type == 0) { 
-                if (y == 0 || contains(builds, x, y - 1, 0) || contains(builds, x - 1, y, 1) || contains(builds, x, y, 1)) {
-                    continue;
+            if (type == 0) {
+                if (!(y == 0 || contains(builds, x, y - 1, 0) || contains(builds, x - 1, y, 1) || contains(builds, x, y, 1))) {
+                    return false;
                 }
-                
-                return false;
-            } else if (type == 1) {
-                if (contains(builds, x, y - 1, 0) || contains(builds, x + 1, y - 1, 0) || 
-                    (contains(builds, x - 1, y, 1) && contains(builds, x + 1, y, 1))) {
-                    continue;
+            } else {
+                if (!(contains(builds, x, y - 1, 0) || contains(builds, x + 1, y - 1, 0) || (contains(builds, x - 1, y, 1) && contains(builds, x + 1, y, 1)))) {
+                    return false;
                 }
-                
-                return false;
             }
         }
-        
         return true;
     }
 
     private boolean contains(List<int[]> builds, int x, int y, int type) {
-        for (int[] build : builds) {
-            if (build[0] == x && build[1] == y && build[2] == type) {
+        for (int[] b : builds) {
+            if (b[0] == x && b[1] == y && b[2] == type) {
                 return true;
             }
         }
-        
         return false;
     }
 }
