d8451af082e68fa485209b54fd5bd6b4f8b155f4
Minjoon Park <alswns11346@gmail.com>
2024-10-14 20:57:12 +0900

    [level 3] Title: 베스트앨범, Time: 4.46 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genrePlayCount = new HashMap<>();
+        int[] answer = {};
+        Map<String, Integer> genreCountPlay = new HashMap<>();
         Map<String, List<int[]>> genreSongs = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
             String genre = genres[i];
             int play = plays[i];
-            int index = i;
+            int idx = i;
 
-            genrePlayCount.put(genre, genrePlayCount.getOrDefault(genre, 0) + play);
-
+            genreCountPlay.put(genre, genreCountPlay.getOrDefault(genre, 0) + play);
             genreSongs.putIfAbsent(genre, new ArrayList<>());
-            genreSongs.get(genre).add(new int[]{index, play});
+            genreSongs.get(genre).add(new int[]{idx, play});
         }
 
-        List<String> sortedGenres = new ArrayList<>(genrePlayCount.keySet());
-        sortedGenres.sort((a, b) -> genrePlayCount.get(b) - genrePlayCount.get(a));
+        List<String> sortedGenres = new ArrayList<>(genreCountPlay.keySet());
+        sortedGenres.sort((a, b) -> genreCountPlay.get(b) - genreCountPlay.get(a));
 
-        List<Integer> bestAlbum = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
-        for (String genre : sortedGenres) {
+        for (String genre : sortedGenres){
             List<int[]> songs = genreSongs.get(genre);
-
-            songs.sort((a, b) -> b[1] - a[1] == 0 ? a[0] - b[0] : b[1] - a[1]);
-
-            for (int i = 0; i < Math.min(songs.size(), 2); i++) {
-                bestAlbum.add(songs.get(i)[0]);
-            }
+            songs.sort((a, b) -> a[1] == b[1] ? a[0] - b[0] : b[1] - a[1]);
+            
+            for (int i = 0; i < Math.min(songs.size(), 2); i++) result.add(songs.get(i)[0]);
         }
-
-        return bestAlbum.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
