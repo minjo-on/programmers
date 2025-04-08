daebb665437f2eef0b63cc3683c96939dea469d4
Minjoon Park <alswns11346@gmail.com>
2025-04-08 17:45:22 +0900

    [level 4] Title: 징검다리, Time: 39.96 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        rocks = Arrays.copyOf(rocks, rocks.length + 1);
+        rocks[rocks.length - 1] = distance;
+        Arrays.sort(rocks);
+        
+        int left = 1;
+        int right = distance + 1;
+        int answer = 1;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            
+            if(isValid(mid, rocks, n)){
+                left = mid + 1;
+                answer = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private boolean isValid(int mid, int[] rocks, int n){
+        int removed = 0;
+        int prev = 0;
+        
+        for(int rock : rocks){
+            if(rock - prev < mid){
+                removed++;
+                continue;
+            }
+            prev = rock;
+        }
+        
+        return removed <= n;
+    }
+}
