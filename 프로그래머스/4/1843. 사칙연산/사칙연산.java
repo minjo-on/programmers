54ce8ac25a659b997c9d7df526c482eda3c5bdcb
Minjoon Park <alswns11346@gmail.com>
2025-04-09 18:08:49 +0900

    [level 4] Title: 사칙연산, Time: 16.56 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -3,21 +3,22 @@ class Solution {
         int n = arr.length / 2 + 1;
         int[][] max = new int[n][n];
         int[][] min = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
+        
+        for(int i = 0; i < n; i++) {
             int num = Integer.parseInt(arr[i * 2]);
             max[i][i] = num;
             min[i][i] = num;
         }
-
-        for (int length = 1; length < n; length++) {
-            for (int i = 0; i < n - length; i++) {
+        
+        for(int length = 1; length < n; length++) {
+            for(int i = 0; i < n - length; i++){
                 int j = i + length;
                 max[i][j] = Integer.MIN_VALUE;
                 min[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k < j; k++) {
+                
+                for(int k = i; k < j; k++) {
                     char op = arr[k * 2 + 1].charAt(0);
-                    if (op == '+') {
+                    if(op == '+'){
                         max[i][j] = Math.max(max[i][j], max[i][k] + max[k + 1][j]);
                         min[i][j] = Math.min(min[i][j], min[i][k] + min[k + 1][j]);
                     } else {
