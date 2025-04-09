9faf97b688602b10840b43af2baebe8cc250aa69
Minjoon Park <alswns11346@gmail.com>
2025-04-09 14:07:56 +0900

    [level 3] Title: 등굣길, Time: 0.84 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -3,29 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n][m];
-
-        boolean[][] puddleMap = new boolean[n][m];
-        for (int[] puddle : puddles) {
-            puddleMap[puddle[1] - 1][puddle[0] - 1] = true;
-        }
-
         dp[0][0] = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (puddleMap[i][j]) {
+        
+        boolean[][] puddleMap = new boolean[n][m];
+        
+        for (int[] puddle : puddles) puddleMap[puddle[1] - 1][puddle[0] - 1] = true;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(puddleMap[i][j]){
                     dp[i][j] = 0;
+                } else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
                 } else {
-                    if (i > 0) {
-                        dp[i][j] += dp[i-1][j] % 1000000007;
-                    }
-                    if (j > 0) {
-                        dp[i][j] += dp[i][j-1] % 1000000007;
-                    }
+                    dp[i][j] = ((i > 0 ? dp[i - 1][j] : 0) + 
+                                (j > 0 ? dp[i][j - 1] : 0)) % 1000000007;
                 }
             }
         }
 
-        return dp[n-1][m-1] % 1000000007;
+        return dp[n-1][m-1];
     }
 }
