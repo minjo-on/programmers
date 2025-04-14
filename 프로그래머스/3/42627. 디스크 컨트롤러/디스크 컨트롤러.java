591daa86a3042d53e7867ccf42e265d1536b7fd5
Minjoon Park <alswns11346@gmail.com>
2025-04-15 02:32:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.01 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,27 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int totalWaitTime = 0;
-        int jobIndex = 0;
+        int total = 0;
+        int jobIdx = 0;
         int second = 0;
-
-        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-
-        while (!priorityQueue.isEmpty() || jobIndex < jobs.length) {
-            while (jobIndex < jobs.length && jobs[jobIndex][0] <= second) {
-                priorityQueue.offer(jobs[jobIndex++]);
+        
+        Arrays.sort(jobs, (a1, a2) -> Integer.compare(a1[0], a2[0]));
+        
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a1, a2) -> Integer.compare(a1[1], a2[1]));
+        
+        while(!queue.isEmpty() || jobIdx < jobs.length){
+            while(jobIdx < jobs.length && jobs[jobIdx][0] <= second) {
+                queue.offer(jobs[jobIdx++]);
             }
-
-            if (priorityQueue.isEmpty()) {
-                second = jobs[jobIndex][0];
+            
+            if(queue.isEmpty()) {
+                second = jobs[jobIdx][0];
             } else {
-                int[] currentJob = priorityQueue.poll();
-                second += currentJob[1];
-                totalWaitTime += second - currentJob[0];
+                int[] current = queue.poll();
+                second += current[1];
+                total += second - current[0];
             }
+            
         }
-
-        return totalWaitTime / jobs.length;
+        
+        return total / jobs.length;
     }
 }
