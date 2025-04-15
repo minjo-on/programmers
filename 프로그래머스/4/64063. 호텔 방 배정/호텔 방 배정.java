e1bda2b257327cfba6879411c4e14f233f18e327
Minjoon Park <alswns11346@gmail.com>
2025-04-15 16:20:43 +0900

    [level 4] Title: 호텔 방 배정, Time: 427.61 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 class Solution {
-    private HashMap<Long, Long> roomMap = new HashMap<>();
-
+    Map<Long, Long> roomMap = new HashMap<>();
+    
     public long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-
-        for (int i = 0; i < room_number.length; i++) {
-            answer[i] = findAvailableRoom(room_number[i]);
+        
+        for(int i = 0; i < room_number.length; i++) {
+            answer[i] = findRoom(room_number[i]);
         }
-
+        
         return answer;
     }
-
-    private long findAvailableRoom(long room) {
-        if (!roomMap.containsKey(room)) {
-            roomMap.put(room, room + 1); 
+    
+    private long findRoom(long room) {
+        if(!roomMap.containsKey(room)){
+            roomMap.put(room, room + 1);
             return room;
         }
-
-        long nextRoom = findAvailableRoom(roomMap.get(room));
+        
+        long nextRoom = findRoom(roomMap.get(room));
         roomMap.put(room, nextRoom);
         return nextRoom;
     }
