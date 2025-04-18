2d3ef090726c72a4d83ca12647310c59448fa4d9
Minjoon Park <alswns11346@gmail.com>
2025-04-18 20:45:32 +0900

    [level 3] Title: 단어 변환, Time: 0.63 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,45 @@
 import java.util.*;
 
-class Word{
-        String word;
-        int count;
+class Word {
+    String word;
+    int count;
 
-        Word(String word, int count){
-            this.word = word;
-            this.count = count;
-        }
+    Word(String word, int count) {
+        this.word = word;
+        this.count = count;
     }
+}
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        if (!Arrays.asList(words).contains(target)) return 0;
 
+        Set<String> visited = new HashSet<>();
         Queue<Word> queue = new LinkedList<>();
-        queue.offer(new Word(begin,0));
-        boolean[] visited = new boolean[words.length];
+        queue.offer(new Word(begin, 0));
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Word current = queue.poll();
-            if (current.word.equals(target)) {
-                return current.count;
-            }
 
-            
-            for(int i = 0; i < words.length; i++){
-                if(visited[i]) continue;
-                int c = 0;
-                for(int j = 0; j < current.word.length(); j++){
-                    if(current.word.charAt(j) != words[i].charAt(j)){
-                        c++;
-                        if (c > 1) break;
-                    }
-                }
-                if(c == 1){
-                    visited[i] = true;
-                    queue.offer(new Word(words[i], current.count + 1));
+            if (current.word.equals(target)) return current.count;
+
+            for (String next : words) {
+                if (!visited.contains(next) && isConvertible(current.word, next)) {
+                    visited.add(next);
+                    queue.offer(new Word(next, current.count + 1));
                 }
             }
         }
+
         return 0;
+    }
+
+    private boolean isConvertible(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) diff++;
+            if (diff > 1) return false;
+        }
+        return diff == 1;
     }
 }
