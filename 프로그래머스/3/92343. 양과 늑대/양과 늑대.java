faf499dbf735d939362c12b008599b00de1298dc
Minjoon Park <alswns11346@gmail.com>
2025-04-19 23:09:57 +0900

    [level 3] Title: 양과 늑대, Time: 3.66 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,44 +1,35 @@
 import java.util.*;
 
 class Solution {
-     private int answer = 0;
-
+    int maxSheep = 0;
+    
     public int solution(int[] info, int[][] edges) {
         List<Integer>[] tree = new ArrayList[info.length];
-        for (int i = 0; i < info.length; i++) {
-            tree[i] = new ArrayList<>();
-        }
-
-        for (int[] edge : edges) {
-            tree[edge[0]].add(edge[1]);
-        }
-
+        
+        for(int i = 0; i < info.length; i++) tree[i] = new ArrayList<>();
+        
+        for(int[] edge : edges) tree[edge[0]].add(edge[1]);
+        
         List<Integer> nextNodes = new ArrayList<>();
         nextNodes.add(0);
-
-        dfs(0, 0, 0, info, tree, nextNodes);
-        return answer;
+        
+        dfs(0, 0, 0, tree, nextNodes, info);
+        
+        return maxSheep;
     }
-
-    private void dfs(int node, int sheep, int wolves, int[] info, List<Integer>[] tree, List<Integer> nextNodes) {
-        if (info[node] == 0) {
-            sheep++;
-        } else {
-            wolves++;
-        }
-
-        if (wolves >= sheep) {
-            return;
-        }
-
-        answer = Math.max(answer, sheep);
-
-        List<Integer> newNextNodes = new ArrayList<>(nextNodes);
-        newNextNodes.remove(Integer.valueOf(node));
-        newNextNodes.addAll(tree[node]);
-
-        for (int nextNode : newNextNodes) {
-            dfs(nextNode, sheep, wolves, info, tree, newNextNodes);
-        }
+    
+    private void dfs(int node, int s, int w, List<Integer>[] tree, List<Integer> nextNodes, int[] info) {
+        if(info[node] == 0) s++;
+        else w++;
+        
+        if(w >= s) return;
+        
+        maxSheep = Math.max(maxSheep, s);
+        
+        List<Integer> next = new ArrayList<>(nextNodes);
+        next.remove(Integer.valueOf(node));
+        next.addAll(tree[node]);
+        
+        for(int n : next) dfs(n, s, w, tree, next, info);
     }
 }
