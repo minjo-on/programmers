4adfb95b393bda4f5d1733e979fecd4200a2d009
Minjoon Park <alswns11346@gmail.com>
2025-07-10 14:42:36 +0900

    [level 3] Title: N으로 표현, Time: 39.52 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -4,27 +4,28 @@ class Solution {
     public int solution(int N, int number) {
         if(N == number) return 1;
 
-        List<List<Integer>> list = new ArrayList<>();
+        List<Set<Integer>> dp = new ArrayList<>();
 
-        for (int i = 1; i < 9; i++){
-            list.add(new ArrayList<>());
-            String repeat = String.valueOf(N).repeat(i);
-            list.get(i - 1).add(Integer.parseInt(repeat));
+        for (int i = 0; i < 8; i++){
+            dp.add(new HashSet<>());
         }
 
-        for (int i = 1; i < 8; i++){
+        for (int i = 0; i < 8; i++){
+            String numStr = String.valueOf(N).repeat(i + 1);
+            dp.get(i).add(Integer.parseInt(numStr));
+            
             for (int j = 0; j < i; j++){
-                for (int a : list.get(j)){
-                    for (int b : list.get(i - j - 1)){
-                        list.get(i).add(a + b);
-                        list.get(i).add(a - b);
-                        list.get(i).add(a * b);
-                        if (b != 0) list.get(i).add(a / b);
+                for (int a : dp.get(j)){
+                    for (int b : dp.get(i - j - 1)){
+                        dp.get(i).add(a + b);
+                        dp.get(i).add(a - b);
+                        dp.get(i).add(a * b);
+                        if (b != 0) dp.get(i).add(a / b);
                     }
                 }
                 
-            }
-            if (list.get(i).contains(number)) return i + 1;
+            } 
+            if (dp.get(i).contains(number)) return i + 1;
         }
         return -1;
     }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
 
 ### 성능 요약
 
-메모리: 312 MB, 시간: 464.33 ms
+메모리: 94.2 MB, 시간: 39.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 19일 16:02:31
+2025년 07월 10일 14:42:32
 
 ### 문제 설명
 
